232002d33603034ec33af6c789c8b02cb09183f9
alsowentafk <alsowentafk@gmail.com>
2020-01-14 13:13:35 +0200

    try with ip

--- front/src/app/Services/tournament.service.ts
+++ front/src/app/Services/tournament.service.ts
@@ -23,7 +23,7 @@ export class TournamentService {
     params: null
   };
    async getAllTournaments(): Promise<Tournament[]> {
-    const endpoint = 'http://localhost:8080/api/tournament/getAllTournaments';
+    const endpoint = 'http://176.114.8.113:8080/api/tournament/getAllTournaments';
     return await this.httpClient.get<Tournament[]>(endpoint).toPromise();
   }
   async findTournamentById(id: string): Promise<Tournament>{
