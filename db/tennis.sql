735012a0de9bbe15bac47808c8a56e5495032bc6
alsowentafk <alsowentafk@gmail.com>
2020-03-08 21:28:45 +0200

    update

--- db/tennis.sql
+++ db/tennis.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Linux (x86_64)
 --
 -- Host: localhost    Database: tennis
 -- ------------------------------------------------------
--- Server version	8.0.18
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,8 +23,8 @@ DROP TABLE IF EXISTS `confirmation_token`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `confirmation_token` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
   `created_date` datetime NOT NULL,
   `confirmation_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
@@ -50,12 +50,12 @@ DROP TABLE IF EXISTS `game`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `game` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `first_player` bigint(20) NOT NULL,
-  `second_player` bigint(20) NOT NULL,
-  `first_score` int(11) DEFAULT NULL,
-  `second_score` int(11) DEFAULT NULL,
-  `winner` int(1) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `first_player` bigint NOT NULL,
+  `second_player` bigint NOT NULL,
+  `first_score` int DEFAULT NULL,
+  `second_score` int DEFAULT NULL,
+  `winner` int DEFAULT NULL,
   `completed` tinyint(1) NOT NULL,
   `type` varchar(55) NOT NULL,
   PRIMARY KEY (`id`)
@@ -80,7 +80,7 @@ DROP TABLE IF EXISTS `tournament`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tournament` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `regulations` longtext NOT NULL,
   `date_start` datetime NOT NULL,
@@ -97,7 +97,7 @@ CREATE TABLE `tournament` (
 
 LOCK TABLES `tournament` WRITE;
 /*!40000 ALTER TABLE `tournament` DISABLE KEYS */;
-INSERT INTO `tournament` VALUES (42,'GENERATION – 2012 – Хмельницький (СХІД)','https://docs.google.com/document/d/e/2PACX-1vSbgEzSRQlgC6_8uI6LNSdIpkCtabSIZdTT560n0pviZQ8WcAaPddYZ1gzyNVwksfKjXdlAaoMA3Om5U--iF2E/pub?embedded=true','2020-01-04 02:00:00','2020-01-03 02:00:00','2020-01-02 02:00:00','https://docs.google.com/document/d/e/2PACX-1vRZisaqzWrSJYv0Lm5vh8OTPjYu41anyk4jyQ-VQu-afV_WgAW3EnpdBzPOaGYeWwy2HKbGdhlCBvSO3y9sqrs/pub?embedded=true'),(43,'GENERATION – 2012 – Черкаси(СХІД)','https://docs.google.com/document/d/e/2PACX-1vQexL_d0ypiUjWuLfcx6L3rx4XkfmmmH_zcEBqlHQMgkUOmIA6TjHAaK2KN9kZB1lVmKaEDlHaYKixOW2rsi7w/pub?embedded=true','2020-01-11 02:00:00','2020-01-10 02:00:00','2020-01-09 02:00:00',''),(44,'Australian Open 2011 – by Oleksandr Dolgopolov','https://docs.google.com/document/d/e/2PACX-1vQgQcNF2g66hATRgQNQvkKa2-8VcXBgXRYQgaCfR5UPXgYvyDOfBnIJvm7oZg1gTgCan66djYx0-1Tdh1cBedY/pub?embedded=true','2020-02-01 02:00:00','2020-01-30 02:00:00','2020-01-29 02:00:00','');
+INSERT INTO `tournament` VALUES (42,'first_tournament','https://docs.google.com/document/d/e/2PACX-1vSbgEzSRQlgC6_8uI6LNSdIpkCtabSIZdTT560n0pviZQ8WcAaPddYZ1gzyNVwksfKjXdlAaoMA3Om5U--iF2E/pub?embedded=true','2020-01-04 02:00:00','2020-01-03 02:00:00','2020-01-02 02:00:00','https://docs.google.com/document/d/e/2PACX-1vRZisaqzWrSJYv0Lm5vh8OTPjYu41anyk4jyQ-VQu-afV_WgAW3EnpdBzPOaGYeWwy2HKbGdhlCBvSO3y9sqrs/pub?embedded=true'),(43,'second_tournament','https://docs.google.com/document/d/e/2PACX-1vQexL_d0ypiUjWuLfcx6L3rx4XkfmmmH_zcEBqlHQMgkUOmIA6TjHAaK2KN9kZB1lVmKaEDlHaYKixOW2rsi7w/pub?embedded=true','2020-01-11 02:00:00','2020-01-10 02:00:00','2020-01-09 02:00:00',''),(44,'third_tournament','https://docs.google.com/document/d/e/2PACX-1vQgQcNF2g66hATRgQNQvkKa2-8VcXBgXRYQgaCfR5UPXgYvyDOfBnIJvm7oZg1gTgCan66djYx0-1Tdh1cBedY/pub?embedded=true','2020-02-01 02:00:00','2020-01-30 02:00:00','2020-01-29 02:00:00','');
 /*!40000 ALTER TABLE `tournament` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,9 +109,9 @@ DROP TABLE IF EXISTS `tournament_game`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tournament_game` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `tournament_id` bigint(20) NOT NULL,
-  `game_id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `tournament_id` bigint NOT NULL,
+  `game_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tournament_match_tournament_id_fk` (`tournament_id`),
   KEY `tournament_game_game_id_fk` (`game_id`),
@@ -137,9 +137,9 @@ DROP TABLE IF EXISTS `tournament_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tournament_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `tournament_id` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `tournament_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
   `birth_certificate` varchar(255) NOT NULL,
   `pay_certificate` varchar(255) DEFAULT NULL,
   `is_confirmed` tinyint(1) NOT NULL,
@@ -170,7 +170,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `email` varchar(55) NOT NULL,
   `is_confirmed` tinyint(1) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -192,7 +192,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin@gmail.com',1,'k2F-KywQq5*!w9K4','blob','Володимир','Кружко','Степанович','2019-05-31 17:09:16','Хмельницький','right','admin'),(2,'',1,'','unknown.jpg','Ілля','Снаксарьов','','2012-01-12 14:15:06','Хмельницький','right','user'),(3,'',1,'','unknown.jpg','Аглая','Аксьонова','','2013-11-07 14:16:05','Львів','right','user'),(4,'',1,'','unknown.jpg','Макар','Гураль','','2012-09-20 14:17:57','Львів','right','user'),(5,'',1,'','unknown.jpg','Лев','Маевський','','2013-08-02 14:19:11','Львів','right','user'),(6,'',1,'','unknown.jpg','Владислав','Скаб','','2013-03-26 14:19:40','Львів','right','user');
+INSERT INTO `user` VALUES (1,'admin@gmail.com',1,'k2F-KywQq5*!w9K4','blob','admin','admin','admin','2019-05-31 17:09:16','Хмельницький','right','admin'),(2,'',1,'','unknown.jpg','Ілля','Корчинський','','2012-01-12 14:15:06','Хмельницький','right','user'),(3,'',1,'','unknown.jpg','Іван','Пономарьов','','2013-11-07 14:16:05','Львів','right','user'),(4,'',1,'','unknown.jpg','Макар','Макаров','','2012-09-20 14:17:57','Львів','right','user'),(5,'',1,'','unknown.jpg','Лев','Іванич','','2013-08-02 14:19:11','Львів','right','user'),(6,'',1,'','unknown.jpg','Владислав','Богомаз','','2013-03-26 14:19:40','Львів','right','user');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -205,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-15 23:44:41
+-- Dump completed on 2020-03-08 19:20:12
